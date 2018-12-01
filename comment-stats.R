9a990a4bf6d46c336f13ec1e7b8d83e49a737dc5
judgelord <devin.jl@gmail.com>
2018-11-30 20:18:59 -0600

    add comment stats and revise mass comments fig

---
+++ comment-stats.R
@@ -0,0 +1,31 @@
+source(here("setup.R"))
+
+# Load all comments as all
+load(here("data/allcomments.Rdata"))
+
+total <- sum(all$numberOfCommentsReceived)
+save( total , file = "totalcomments.tex")
+
+rins <- length(unique(all$rin))
+save( rins, file = "RINcount.tex")
+
+massRINs <- filter(all, numberOfCommentsReceived > 100)
+
+total <- sum(massRINs$numberOfCommentsReceived)
+total
+save( total , file = "masscommentsTotal.tex")
+
+massRINs <- unique(massRINs$rin)
+total <-  length(unique(massRINs$rin))
+total
+save( total , file = "massRINcount.tex")
+
+
+massRINs <- filter(all, rin %in% massRINs)
+
+save( massRINs , file = "massRINs.Rdata")
+
+total <- sum(massRINs$numberOfCommentsReceived)
+total
+save( total , file = "massRINsTotal.tex")
+
--- mergecomments.R
+++ mergecomments.R
@@ -1,87 +1,62 @@
-last <- d
-all <- last 
+library(here)
+source(here("rulemaking/setup.R"))
+load(here("ascending/lastcomments.Rdata"))
+all <- d
 
-load("500comments.Rdata")
+load(here("ascending/9000comments.Rdata"))
 all %<>% full_join(d)
 
-load("1000comments.Rdata")
+load(here("ascending/8500comments.Rdata"))
 all %<>% full_join(d)
 
-load("1500comments.Rdata")
+load(here("ascending/8000comments.Rdata"))
 all %<>% full_join(d)
 
-load("2000comments.Rdata")
+load(here("ascending/7500comments.Rdata"))
 all %<>% full_join(d)
 
-load("2500comments.Rdata")
+load(here("ascending/7000comments.Rdata"))
 all %<>% full_join(d)
 
-load("3000comments.Rdata")
+load(here("ascending/6500comments.Rdata"))
 all %<>% full_join(d)
 
-load("3500comments.Rdata")
+load(here("ascending/6000comments.Rdata"))
 all %<>% full_join(d)
 
-load("4000comments.Rdata")
+load(here("ascending/5500comments.Rdata"))
 all %<>% full_join(d)
 
-load("4500comments.Rdata")
+load(here("ascending/5000comments.Rdata"))
 all %<>% full_join(d)
 
-load("5000comments.Rdata")
+load(here("ascending/4500comments.Rdata"))
 all %<>% full_join(d)
 
-load("5500comments.Rdata")
+load(here("ascending/4000comments.Rdata"))
 all %<>% full_join(d)
 
-load("6000comments.Rdata")
+load(here("ascending/3500comments.Rdata"))
 all %<>% full_join(d)
 
-load("6500comments.Rdata")
+load(here("ascending/3000comments.Rdata"))
 all %<>% full_join(d)
 
-load("7000comments.Rdata")
+load(here("ascending/2500comments.Rdata"))
 all %<>% full_join(d)
 
-load("7500comments.Rdata")
+load(here("ascending/2000comments.Rdata"))
 all %<>% full_join(d)
 
-load("8000comments.Rdata")
+load(here("ascending/1500comments.Rdata"))
 all %<>% full_join(d)
 
-load("8500comments.Rdata")
+load(here("ascending/1000comments.Rdata"))
 all %<>% full_join(d)
 
-load("9000comments.Rdata")
+load(here("ascending/500comments.Rdata"))
 all %<>% full_join(d)
 
-save(all, file = "allcomments.Rdata")
 #####################################
-load("allcomments.Rdata")
-
-total <- sum(all$numberOfCommentsReceived)
-save( total , file = "totalcomments.tex")
-
-rins <- length(unique(all$rin))
-save( rins, file = "RINcount.tex")
-
-massRINs <- filter(all, numberOfCommentsReceived > 100)
-
-total <- sum(massRINs$numberOfCommentsReceived)
-total
-save( total , file = "masscommentsTotal.tex")
-
-massRINs <- unique(massRINs$rin)
-total <-  length(unique(massRINs$rin))
-total
-save( total , file = "massRINcount.tex")
-
-
-massRINs <- filter(all, rin %in% massRINs)
-
-save( massRINs , file = "massRINs.Rdata")
-
-total <- sum(massRINs$numberOfCommentsReceived)
-total
-save( total , file = "massRINsTotal.tex")
-
+save(all, file = "ascending/allcomments.Rdata")
+#####################################
