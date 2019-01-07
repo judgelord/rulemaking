1e565145f0dc1255421d09554a33a16a502f6385
judgelord <devin.jl@gmail.com>
2019-01-07 14:35:13 -0600

    cluster comments

---
+++ clustercomments.R
@@ -0,0 +1,171 @@
+library(here)
+source(here("setup.R"))
+# load(here("ascending/allcomments.Rdata"))
+d<- all
+
+d %<>% group_by(docketId) %>% 
+  mutate(docketTotal = sum(numberOfCommentsReceived)) %>% 
+  ungroup()
+
+d %<>% 
+  # logical
+  mutate(comment = nchar(commentText) > 140 ) %>% 
+  # text removing short phrases
+  mutate(commenttext =
+           ifelse( comment, commentText, NA)
+         ) %>% 
+  mutate(commentfirst140 = str_sub(commenttext,1,140)) %>% 
+  mutate(commentlast140 = str_sub(commenttext,-140,-1))
+
+# count types 
+d %<>% 
+  group_by(docketId, commenttext) %>% 
+  mutate(commentsIdentical = ifelse(comment, n(), NA) ) %>%
+  ungroup() %>% 
+  group_by(docketId, commentfirst140) %>% 
+  mutate(commentsPartial = ifelse(comment, n(), NA) ) %>% 
+  ungroup() %>% 
+  group_by(docketId, commentlast140) %>% 
+  mutate(commentsPartial = ifelse(comment, n(), commentsPartial) ) %>% 
+  ungroup()
+  
+# NOTE: SOME OF THESE ARE DUPLICATES THAT NEED TO BE INVESTIGATED (IS REGULATIONS.GOV DOUBLE COUNTING OCCACIONALLY?)
+# d %>% filter(commentsIdentical>2) %>% select(numberOfCommentsReceived, organization, commentText)
+# d %>% filter(commentsPartial>9) %>% select(commentText)
+
+
+
+# define mass
+  d %<>% 
+    # unique comments
+    mutate(mass = ifelse(numberOfCommentsReceived>1 | commentsIdentical>1, "Small batch", "Unique")) %>%
+    # partially unique comments 
+    mutate(mass = ifelse(commentsIdentical==1 & commentsPartial>1, "Partially unique", mass)) %>%
+    # just in case the above counted NAs
+    mutate(mass = ifelse(comment==FALSE, NA, mass))%>% 
+    # bulk submissions over 99 
+    mutate(mass = ifelse(numberOfCommentsReceived>99 | commentsIdentical>99, "Mass Comments", mass))
+
+  # form of comment
+  d %<>% 
+    mutate(commentform = tolower(str_extract_all(title, "email|Email|USB|Paper|paper|Web|web|Postcard|postcard|Change.org|eRulemaking Portal")))
+  
+  
+# extract org info 
+preface <- ".*ponsored by |.*ponsoring organization |.*ubmitted by |.*omments from |.*ampaign from |.*on behalf of "
+
+d %<>% 
+  mutate(organization = 
+           ifelse(grepl(preface,title) &
+                    (is.na(organization) | str_detect(organization, "nknown") ),
+                  gsub(preface,"",title), organization) ) %>%
+  mutate(organization = ifelse( str_detect(organization, "nknown|^NA$|^n/a$"),NA, organization)) 
+
+# clean orgs
+# d %<>% mutate(organization = ifelse(is.na(organization), title, organization) )
+# d$organization <- gsub(".*ponsored by |.*ponsoring organization |.*ampaign from |.*ubmitted by |.*omment from |.* on behalf of ", "", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*MoveOn.*", "MoveOn", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*CREDO.*", "CREDO Action", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*NRDC.*", "Natural Resources Defense Council", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Care2.*", "Care2", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Environmental Working|EWG.*", "Environmental Working Group", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Earthjustice.*", "Earthjustice", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Sierra Club.*", "Sierra Club", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Environmental Action.*", "Environmental Action", d$organization, ignore.case = TRUE)
+d$organization <- gsub("Opportunties", "Opportunities", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*EDF.*", "Environmental Defense Fund", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Environmental Defense.*", "Environmental Defense Fund", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Monsanto.*", "Monsanto", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*American Lung Association.*", "American Lung Association", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Green For All.*", "Green For All", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Biological Diversity.*", "Center for Biological Diversity", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Audubon.*", "National Audubon Society", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Environment Maryland and Environment Virginia.*", "Environment Maryland and Environment Virginia", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Public Interest Research Group.*|.*tudent PIRG.*|.*PIRG..tudent.*", "Public Interest Research Group", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Unitarian*|unitarian.*", "Unitarian Congregations", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*American Policy Center.*", "American Policy Center", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Asbestos Disease Awareness Organization.*", "Asbestos Disease Awareness Organization", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Consumers Union.*", "Consumers Union", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*National Wildlife Federation.*", "National Wildlife Federation", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*350.*", "350", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Conservation Voters.*", "League of Conservation Voters", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Association for Gun Rights.*", "National Association for Gun Rights", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Friends of the Earth.*", "Friends of the Earth", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Center for Effective Government.*", "Center for Effective Government", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Interfaith Power and Light.*", "Interfaith Power and Light", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*Planned Parenthood.*", "Planned Parenthood", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*MomsRising|mom's rising.*", "MomsRising", d$organization, ignore.case = TRUE)
+d$organization <- gsub(".*PEW.*", "PEW", d$organization, ignore.case = FALSE)
+d$organization <- gsub(".*Farm Bureau.*", "Farm Bureau", d$organization, ignore.case = FALSE)
+d$organization <- gsub(".*Organizing for Action.*", "Organizing For Action", d$organization, ignore.case = FALSE)
+
+# second to last
+d$organization <- gsub(".*members of ", "", d$organization, ignore.case = FALSE)
+d$organization <- gsub(".*Change.org.*", "Change.org", d$organization, ignore.case = FALSE)
+d$organization <- gsub(".*unknown.*|^N.A$", "unknown", d$organization, ignore.case = TRUE)
+# do this last 
+d$organization <- gsub("  ", " ", d$organization, ignore.case = TRUE)
+d$organization <- gsub("^the |^ | $", "", d$organization, ignore.case = TRUE)
+d$organization <- gsub("\\(.*", "", d$organization, ignore.case = TRUE)
+d$organization <- gsub("\\. Sample.*|\\. \n.*|\n.*|\\,.*", "", d$organization, ignore.case = TRUE)
+d$organization <- gsub(" et al.*| - .*", "", d$organization, ignore.case = TRUE)
+d$organization <- gsub(" \\(.*| \\[.*", "", d$organization, ignore.case = TRUE)
+
+
+d %<>% 
+  mutate(organization = ifelse(organization %in% c("", "NA", "unknown"), NA, organization)) %>%
+  group_by(organization) %>% 
+  mutate(orgTotal = sum(numberOfCommentsReceived)) %>% 
+  ungroup() %>%
+  arrange(-orgTotal)
+
+# unique(d$organization)
+
+
+
+allcomments2 <- d
+save(allcomments2, file ="ascending/allcomments2.Rdata") 
+
+
+
+
+
+
+
+
+toporgs <- d %>% 
+  filter(!is.na(organization), organization != 'NA') %>% 
+  filter(nchar(organization)<50) %>% 
+  filter(orgTotal %in% sort(
+    unique(d$orgTotal), 
+    decreasing = T)[1:100] 
+  ) %>% 
+  arrange(orgTotal) 
+
+save(toporgs, file = "data/toporgs.Rdata")
+
+
+
+
+
+
+topdockets <- d %>% 
+  filter(docketTotal %in% sort(
+    unique(d$docketTotal), 
+    decreasing = T)[1:10] 
+    )
+
+save(topdockets, file = "data/topdockets.Rdata")
+
+
+
+
+###########################
+# hand coding 
+tocode <- filter(toporgs, docketType == "Rulemaking") %>% 
+  select(numberPerDocket, numberOfCommentsReceived, organization, submitterName, title, commentText, docketTitle) %>%
+  arrange(-numberPerDocket)
+
+write.csv(tocode, here("data/tocode.csv"))
+
+
---
+++ scratchpad/comments.R
@@ -0,0 +1,95 @@
+library(here)
+source(here("setup.R"))
+
+# MOVE TO SUMMARY 
+
+# orgs 
+load(here("data/toporgs.Rdata"))
+
+toporgs$mass %<>% 
+  str_replace("Not unique", "Small batch")
+
+toporgs %>%
+  mutate(topdockets = ifelse(docketTotal %in% sort(unique(toporgs$docketTotal), decreasing = T)[1:10],
+        docketId, NA) ) %>%
+  filter(orgTotal %in% sort(
+    unique(toporgs$orgTotal), 
+    decreasing = T)[1:10] 
+  ) %>% 
+  filter(!is.na(mass)) %>% 
+  ggplot() +
+  geom_col(aes(x = organization, y = numberOfCommentsReceived/1000)) +
+  coord_flip()+
+  facet_grid(. ~ mass, scales = "free_x") + 
+  labs(x = "", y = "Number of Comments (thousands)", fill = "") + theme_bw() 
+
+
+# dockets 
+load(here("data/topdockets.Rdata"))
+
+unique(topdockets$docketTitle)
+# short docket titles
+topdockets$docketTitle %<>%
+  str_replace(
+    "Repeal of Carbon Dioxide Emission Guidelines for Existing Stationary Sources: Electric Utility Generating Units; Emission Guidelines for Greenhouse Gas Emissions from Existing Electric Utility Generating Units; Revisions to Emission Guideline Implementing Regulations; Revisions to New Source Review Program",
+    "2017 Clean Power Plan Repeal"
+  ) %>%
+  str_replace(
+    "Payday, Vehicle, Title and Certain High-Cost Installment Loans",
+    "2016 Payday Loans"
+  ) %>%
+  str_replace(
+    "Standards of Performance for Greenhouse Gas Emissions from Existing Sources: Electric Utility Generating Units",
+    "2013 Clean Power Plan"
+  ) %>%
+  str_replace(
+    "Greenhouse Gas New Source Performance Standard for Electric Generating Units",
+    "2011 Greenhouse Gas NSPS"
+  ) %>%
+  str_replace(
+    "Oil and Natural Gas Sector -- New Source Performance Standards, National Emission Standards for Hazardous Air Pollutants, and Control Techniques Guidelines",
+    "2010 Oil and Gas NSPS"
+  ) %>%
+  str_replace(
+    "Removing the Gray Wolf from the List of Endangered and Threatened Wildlife and Maintaining Protections for the Mexican Wolf by Listing It as Endangered",
+    "2013 Gray Wolf removal"
+  ) %>%
+  str_replace(
+    ".*of the 2019-2024 Draft Proposed Outer Continental Shelf .OCS. Oil and Gas Leasing Program and Notice of Intent .NOI. to Prepare a Programmatic Environmental Impact Statement .EIS.",
+    "2017 Outer Continental Shelf Oil and Gas Leasing"
+  ) %>%
+  str_replace(
+    "Review of Certain National Monuments Established Since 1996; Notice of Opportunity for Public Comment",
+    "2017 National Monuments Review"
+  ) %>%
+  str_replace(
+    "Standards of Performance for Greenhouse Gas Emissions for New Stationary Sources: Electric Utility Generating Units",
+    "2013 Greenhouse Gas Emissions for New Stationary Sources"
+  ) %>%
+  str_replace(
+    "Request for Comments on the Proposed 2017-2022 OCS Oil and Gas Leasing Program.*",
+    "2016 Outer Continental Shelf Oil and Gas Leasing")  
+
+topdockets$mass %<>% 
+  str_replace("Not unique", "Small batch")
+  
+
+topdockets %<>% 
+  group_by(organization) %>%
+  mutate(orgTotal = sum(numberOfCommentsReceived)) %>% 
+  ungroup()
+
+topdockets %>% 
+  filter(!is.na(mass)) %>% 
+  mutate(toporgs = ifelse(orgTotal %in% sort(unique(topdockets$orgTotal), decreasing = T)[1:20],
+                             organization, NA) ) %>%
+  ggplot() +
+  geom_col(aes(x = docketTitle, y = numberOfCommentsReceived/1000, fill = toporgs)) +
+  coord_flip() +
+  facet_grid(. ~ mass, scales = "free_x") + 
+  labs(x = "", y = "Number of Comments (thousands)", fill = "") + 
+  theme_bw()
+
+topdockets %>% group_by(docketId, organization) %>% summarise(n = sum(numberOfCommentsReceived)) %>% arrange(-n)
+
+
