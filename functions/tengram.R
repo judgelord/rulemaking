5eee1a1c1d14f29d6a45fa33546aee368cfb7fef
judgelord <devin.jl@gmail.com>
2020-11-28 11:58:34 -0600

    fixes bug with read_grams function

--- functions/tengram.R
+++ functions/tengram.R
@@ -6,12 +6,11 @@ tengram <- . %>% unnest_tokens(tengram, text, token = "ngrams", n = 10)
 # a meta function to read document, clean it, and parse it into tengrams
 read_grams <- . %>% 
   read_table(col_names = "text") %>%
-  mutate(text = clean_string(text)) %>%
-  tengram() %>% 
+  summarise(text = clean_string(text)) %>%
+  distinct() %>% 
   rownames_to_column() %>% 
-  filter(
-    # limit to 10k wordds 
-    as.numeric(rowname) < 10000) %>% 
+  filter(as.numeric(rowname) < 10000) %>% 
+  tengram() %>% 
   select(tengram) %>% 
   as.list()
 
@@ -32,12 +31,12 @@ match <- function(text1, text2){
 # map2(d$tengrams[3], d$tengrams, match) %>% enframe() %>% mutate(name = d$reg_dot_gov_document_id) 
 # ####################################################################################################
 
-# a function to do this to each 
+# to do this to each 
 match2 <- . %>% map2(d$tengrams, match) #FIXME  supply list of tengrams as a var?
 
 # a function to caputure just one other match (in this case, the diff with the NPRM)
 match_tibble <- function(text1, text2){
-  tibble(text = unlist(text1), 
+  tibble(ngram = unlist(text1), 
          match = unlist(text1) %in% unlist(text2)
   )
 }
@@ -55,7 +54,7 @@ tengram_match_to_text <- function(d){
     mutate(match = ifelse(fix>0 & match == F, T, match)) %>% 
     # add elipses after transition words 
     mutate(word = ifelse(trans, str_c(word, "..."), word) ) %>%
-    select(word, match)
+    select(-trans, -fix)
   return(d)
 }
 
