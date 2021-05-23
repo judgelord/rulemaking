ebd58c1cbe705aae46346e0cc7343dee92bad255
judgelord <devin.jl@gmail.com>
2021-05-22 22:57:04 -0500

    fixes bad comment start, end, and posted dates adds docket_date

--- code/updateRdata.R
+++ code/updateRdata.R
@@ -88,10 +88,9 @@ save(ejcommentsnew, file = here::here("data", "ejcommentsnew.Rdata"))
 
 
 
-
+dim(comment_metadata)
 # subset comments 
-
-load("comment_metadata.Rdata")
+load(here::here("data", "comment_metadata2020.Rdata"))
 names(comment_metadata)
 
 org_comments <- comment_metadata %>% filter(!is.na(organization)) %>% distinct(organization, docket_id, number_of_comments_received)
@@ -102,10 +101,62 @@ dim(org_comments)
 save(org_comments, file = here::here("data", "org_comments.Rdata"))
 
 
+# Minimal comment data 
 
-comments_min <- comment_metadata %>% distinct(id, organization,submitter_name, number_of_comments_received)
+class(comment_metadata$posted_date)
+class(comment_metadata$comment_due_date)
+class(comment_metadata$comment_start_date)
+
+# to Date 
+comment_metadata$comment_due_date %<>% as.Date()
+comment_metadata$comment_start_date %<>% as.Date()
+
+range(comment_metadata$posted_date, na.rm = T)
+range(comment_metadata$comment_due_date, na.rm = T)
+range(comment_metadata$comment_start_date, na.rm = T)
+
+sum(is.na(comment_metadata$posted_date))
+sum(is.na(comment_metadata$comment_due_date))
+sum(is.na(comment_metadata$comment_start_date))
+
+
+# on some older dockets, comments were uploaded later, so date posted not the date submitted, so I approximate with due date 
+comment_metadata %<>%
+  mutate(docket_date = str_extract(docket_id,"(19|20)[0-9][0-9]") %>% str_c("-01-01") %>% as.Date())
+
+range(comment_metadata$docket_date, na.rm = T)
+
+# make NA impossible dates
+comment_metadata %<>% # head() %>%
+  mutate(posted_date = if_else(posted_date < docket_date, as.Date(NA), posted_date),
+         comment_start_date = if_else(comment_start_date < docket_date, as.Date(NA), comment_start_date),
+         comment_due_date = if_else(comment_due_date < docket_date, as.Date(NA), comment_due_date)) #%>% select(ends_with("date"))
+
+comment_metadata %<>%
+  mutate(date = pmin(posted_date, comment_due_date, na.rm = T) %>% 
+           # if still missing fill in comment start date (at least we should get the right admin)
+           coalesce(comment_start_date, docket_date)) %>%
+  ungroup() #%>% select(ends_with("date"))
+
+comment_metadata %<>% arrange(-date)
+
+comment_metadata %>% tail() %>% select(docket_id, ends_with("date"))
+
+range(comment_metadata$date, na.rm = T)
+range(comment_metadata$docket_date, na.rm = T)
+sum(is.na(comment_metadata$docket_date))
+sum(is.na(comment_metadata$date))
+
+comment_metadata %>% filter(is.na(docket_date)) %>% #count(posted_date, sort = T) %>% 
+  head(10)
+
+# trim down to minimals vars 
+comments_min <- comment_metadata %>% distinct(id, organization,submitter_name, number_of_comments_received, date)
 
 head(comments_min)
+dim(comment_metadata)
 dim(comments_min)
 
 save(comments_min, file = here::here("data", "comments_min.Rdata"))
+
+# save(comment_metadata, file =  here::here("data", "comment_metadata2020.Rdata"))
